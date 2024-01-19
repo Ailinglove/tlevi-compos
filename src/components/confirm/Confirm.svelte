496ba1fdf7cb4e3a6d9f341dc4e7201ab75bbce3
sunlliu <sunlliu@tencent.com>
2024-01-19 11:27:45 +0800

    feat: 确认弹窗

--- src/components/confirm/Confirm.svelte
+++ src/components/confirm/Confirm.svelte
@@ -1,8 +1,17 @@
 <script>
-  let title = '确认XXX';
-  let subTitle = ''
-  let okText = '确认'
-  let cancelText = '取消'
+  import { createEventDispatcher } from "svelte";
+  export let title = '确认XXX';
+  export let content = ''
+  export let okText = '确认'
+  export let cancelText = '取消'
+
+  const dispatch = createEventDispatcher();
+  const onCancel = ()=>{
+    dispatch('onCancel')
+  }
+  const onOk = ()=>{
+    dispatch('onOk')
+  }
 
 </script>
 <div class="tlevi-confirm-wrapper">
@@ -10,20 +19,25 @@
     <div class="tlevi-content">
       <div class="tlevi-content-title">{title}</div>
       <div class="tlevi-content-box">
-        {#if subTitle}
-          <div class="tlevi-content-subtitle">{subTitle}</div>
+        {#if content}
+          <div class="tlevi-content-content">{content}</div>
         {:else}
           <slot></slot>
         {/if}
 
       </div>
       <div class="tlevi-content-btn-wrapper">
-        <div class="tlevi-content-btn-ok">
+        {#if okText}
+        <div class="tlevi-content-btn-ok" 
+          on:click={onOk}>
           {okText}
         </div>
-        <div class="tlevi-content-btn-cancel">
+        {/if}
+        {#if cancelText}
+        <div class="tlevi-content-btn-cancel" on:click={onCancel}>
           {cancelText}
         </div>
+        {/if}
 
       </div>
 
@@ -53,23 +67,29 @@
       &-title{
         font-weight: 600;
         font-size: 12px;
+        text-align: center;
+
       }
       &-box{
         padding: 20px 0;
-        .tlevi-content-subtitle{
+        .tlevi-content-content{
           font-size: 11px;
+          text-align: center;
+
         }
       }
       &-btn-wrapper{
         display: flex;
         justify-content: space-around;
         align-items: center;
+
         .tlevi-content-btn-ok,
         .tlevi-content-btn-cancel{
           border-radius: 20px;
           width: 120px;
           height: 40px;
           line-height: 40px;
+          text-align: center;
         }
         .tlevi-content-btn-ok{
           background-color: orange;
--- src/story/tlevi-confirm/tlevi-confirm.story.svelte
+++ src/story/tlevi-confirm/tlevi-confirm.story.svelte
@@ -2,13 +2,27 @@
   import Popup from "../../components/confirm/Confirm.svelte";
   import {logEvent} from 'histoire/client'
   export let Hst
-  let btnContext='这是一个按钮';
-  let disabled = false;
+  let title='删除';
+  let content = '确认删除吗？';
+  let okText = '确认';
+  let cancelText = '取消';
+  let show = false;
 
-  const onClickBtn = (data)=>{
-    console.log(data, data.detail)
-    logEvent('onClickBtn', data)
-    alert(`子组件传过来的参数啊${data.detail.num}`)
+  const onCancel = (e)=>{
+    logEvent('onClickBtn', e);
+    show = false
+    alert(`点击取消`)
+  }
+
+
+  const onOk = (e)=>{
+    logEvent('onOk', e)
+    alert(`点击确认`)
+    show = false
+  }
+
+  const changeConfirmStatus = ()=>{
+    show = !show;
   }
 </script>
 
@@ -16,7 +30,9 @@
   title="二次确认弹窗"
 >
   <Hst.Variant>
-    <Popup>
+    <button on:click={changeConfirmStatus}>点击展示弹窗</button>
+    {#if show}
+    <Popup {title} {content} {okText} {cancelText} on:onOk={onOk} on:onCancel={onCancel}>
       <p>这是中间的内容啊啊啊啊啊啊啊</p>
       <p>这是中间的内容啊啊啊啊啊啊啊</p>
       <p>这是中间的内容啊啊啊啊啊啊啊</p>
@@ -24,17 +40,14 @@
       <p>这是中间的内容啊啊啊啊啊啊啊</p>
       <p>这是中间的内容啊啊啊啊啊啊啊</p>
       <p>这是中间的内容啊啊啊啊啊啊啊</p>
-
     </Popup>
-
-    <!-- <svelte:fragment slot="controls">
-      Content: <input type="text" bind:value={btnContext} />
-      Disabled: <input type="checkbox" bind:checked={disabled} />
-    </svelte:fragment> -->
+    {/if}
 
     <svelte:fragment slot="controls">
-      <Hst.Text bind:value={btnContext} title="Content" />
-      <Hst.Checkbox bind:value={disabled} title="Disabled" />
+      <Hst.Text bind:value={title} title="title" />
+      <Hst.Text bind:value={content} title="content" />
+      <Hst.Text bind:value={okText} title="okText" />
+      <Hst.Text bind:value={cancelText} title="cancelText" />
     </svelte:fragment>
   </Hst.Variant>
 </Hst.Story>
