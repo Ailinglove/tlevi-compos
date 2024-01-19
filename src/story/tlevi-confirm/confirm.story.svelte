f734762eb114fa1784a9d55c9c240d899b2aa628
sunlliu <sunlliu@tencent.com>
2024-01-19 11:54:04 +0800

    fix: gengxin

--- src/story/tlevi-confirm/confirm.story.svelte
+++ src/story/tlevi-confirm/confirm.story.svelte
@@ -10,15 +10,13 @@
 
   const onCancel = (e)=>{
     logEvent('onClickBtn', e);
-    show = false
-    alert(`点击取消`)
+    show = false;
   }
 
 
   const onOk = (e)=>{
     logEvent('onOk', e)
-    alert(`点击确认`)
-    show = false
+    show = false;
   }
 
   const changeConfirmStatus = ()=>{
@@ -29,17 +27,31 @@
 <Hst.Story
   title="二次确认弹窗"
 >
-  <Hst.Variant>
+  <Hst.Variant title="有content">
     <button on:click={changeConfirmStatus}>点击展示弹窗</button>
     {#if show}
     <Popup {title} {content} {okText} {cancelText} on:onOk={onOk} on:onCancel={onCancel}>
-      <p>这是中间的内容啊啊啊啊啊啊啊</p>
-      <p>这是中间的内容啊啊啊啊啊啊啊</p>
-      <p>这是中间的内容啊啊啊啊啊啊啊</p>
-      <p>这是中间的内容啊啊啊啊啊啊啊</p>
-      <p>这是中间的内容啊啊啊啊啊啊啊</p>
-      <p>这是中间的内容啊啊啊啊啊啊啊</p>
-      <p>这是中间的内容啊啊啊啊啊啊啊</p>
+    </Popup>
+    {/if}
+
+    <svelte:fragment slot="controls">
+      <Hst.Text bind:value={title} title="title" />
+      <Hst.Text bind:value={content} title="content" />
+      <Hst.Text bind:value={okText} title="okText" />
+      <Hst.Text bind:value={cancelText} title="cancelText" />
+    </svelte:fragment>
+  </Hst.Variant>
+  <Hst.Variant title="自定义content">
+    <button on:click={changeConfirmStatus}>点击展示弹窗</button>
+    {#if show}
+    <Popup {title} {okText} {cancelText} on:onOk={onOk} on:onCancel={onCancel}>
+      <p>这是用户自定义的内容！！</p>
+      <p>这是用户自定义的内容！！</p>
+      <p>这是用户自定义的内容！！</p>
+      <p>这是用户自定义的内容！！</p>
+      <p>这是用户自定义的内容！！</p>
+      <p>这是用户自定义的内容！！</p>
+      <p>这是用户自定义的内容！！</p>
     </Popup>
     {/if}
 
